4440442737c2b2150db8e864fd6ad9b27fcf0abb
Indy Reynders <113059997+IndyR7@users.noreply.github.com>
2023-03-13 21:03:12 +0100

    My solution to the Desktop Calculator-project from Hyperskill

---
+++ Desktop Calculator/Topics/Functional interfaces/A lambda expression with seven arguments/src/Main.java
@@ -0,0 +1,8 @@
+class Seven {
+    public static SeptenaryStringFunction fun = str -> String.join("", str).toUpperCase();
+}
+
+@FunctionalInterface
+interface SeptenaryStringFunction {
+    String apply(String... str);
+}
