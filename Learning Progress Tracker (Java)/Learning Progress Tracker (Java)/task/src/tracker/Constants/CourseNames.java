8b05d817ac164afc4d1d895be4eec63cac93e345
Indy Reynders <113059997+IndyR7@users.noreply.github.com>
2023-05-13 21:53:36 +0200

    Delete Learning Progress Tracker (Java) directory

--- Learning Progress Tracker (Java)/Learning Progress Tracker (Java)/task/src/tracker/Constants/CourseNames.java
+++
@@ -1,8 +0,0 @@
-package tracker.Constants;
-
-public class CourseNames {
-    public static final String JAVA = "Java";
-    public static final String DSA = "DSA";
-    public static final String DATABASES = "Databases";
-    public static final String SPRING = "Spring";
-}
